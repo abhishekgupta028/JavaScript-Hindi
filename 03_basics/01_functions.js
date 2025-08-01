d4cb0b59b713c2c317e18bb778cbf574e0756e0e
abhishekgupta028 <abhishekgupta6205691283@gmail.com>
2025-08-01 16:16:08 +0000

    Functions in JS Intro

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,47 @@
+function sayMyName(){
+    console.log("A")
+    console.log("b")
+    console.log("h")
+    console.log("i")
+}
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//      console.log(number1 + number2)
+// }
+
+// addTwoNumbers(2,4)
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(4,6)
+
+//console.log("Result: ", result)
+
+function userLoginMessage(username){
+    if(!username){
+        console.log("Please enter a username")
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(userLoginMessage("Abhishek"))
+// console.log(userLoginMessage())
+
+function userLoginMessage(username = "Tushar"){
+    if(!username){
+        console.log("Please enter a username")
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(userLoginMessage("Abhishek"))
+// console.log(userLoginMessage())
+
+
