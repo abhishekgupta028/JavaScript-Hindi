3cefbe64cbe46cca825c37a32e4db3385ed13306
abhishekgupta028 <abhishekgupta6205691283@gmail.com>
2025-08-02 15:38:51 +0000

    scopes and its types

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,24 @@
+// let a = 20
+// const b = 30
+// var c = 40
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+     console.log("INNER: ", a);
+    
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
