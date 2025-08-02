003b0f22bfbbd4fdb8fc0102590b901604909ce7
abhishekgupta028 <abhishekgupta6205691283@gmail.com>
2025-08-02 13:52:45 +0000

    Functions and its Properties in JS

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -44,4 +44,36 @@ function userLoginMessage(username = "Tushar"){
 // console.log(userLoginMessage("Abhishek"))
 // console.log(userLoginMessage())
 
+function calculatePrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculatePrice(200,300,400,500))
+
+console.log(typeof calculatePrice(200,300,400,500))
+
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and Price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
 
